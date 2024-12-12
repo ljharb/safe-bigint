0b384191518b549beaa3f01911f373c970441bbc
Jordan Harband <ljharb@gmail.com>
2024-12-11 19:14:23 -0800

    [Refactor] use `call-bound` directly

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var GetIntrinsic = require('get-intrinsic');
-var callBound = require('call-bind/callBound');
+var callBound = require('call-bound');
 
 var $BigInt = GetIntrinsic('%BigInt%', true);
 var $Function = GetIntrinsic('%Function%');
